e60f9fa22b50f13c4ade8ad539ae61bec15bd95e
jrjflei <jrjflei@gmail.com>
2018-09-25 15:57:56 -0400

    updating Heroku app to master

--- stories.py
+++ stories.py
@@ -4,7 +4,7 @@ from database import Database
 import json
 import datetime
 import random
-
+import keras
 import signal
 
 class timeout:
@@ -64,7 +64,7 @@ def newspaperize(article_url):
 
     print('begin processing')
     headline = article.title
-    top_image = article.top_image
+    imageurl = article.top_image
     timestamp = article.publish_date
     content = article.text
     article.nlp()
@@ -87,7 +87,7 @@ def newspaperize(article_url):
     article_information = OrderedDict([
                   ("id" , id_number),
                   ("name", headline),
-                  ("imageurl", top_image),
+                  ("imageurl", imageurl),
                   ("url" , article_url),
                   ("timestamp" , timestamp.isoformat() if timestamp is not None else ""),
                   ("description" , description),
@@ -108,6 +108,7 @@ def newspaperize(article_url):
 
 from sklearn.externals import joblib
 from scipy.sparse import hstack
+from keras.models import load_model
 import pandas as pd
 
 def classify_clickbait(headline):
@@ -121,13 +122,21 @@ def classify_clickbait(headline):
     
     with open('svm.pkl', 'rb') as f:
         svm = joblib.load(f)
+    with open('mnb.pkl', 'rb') as f:
+        mnb = joblib.load(f)
+    with open('lr.pkl', 'rb') as f:
+        lr = joblib.load(f)
+    with open('rf.pkl', 'rb') as f:
+        rf = joblib.load(f)
+    with open('neural_net.h5', 'rb') as f:
+        neural_net = load_model('neural_net.h5')
+
     with open('tfidf_vectorizer_pos.pkl', 'rb') as f:
         tfidf_vectorizer_pos = joblib.load(f)
     with open('tfidf_vectorizer_text.pkl', 'rb') as f:
         tfidf_vectorizer_text = joblib.load(f)
-    print('models loaded')
     
-    print(svm.__dict__.items())
+    print('models loaded')
     
     headline_pos = getPosTags(headline)
     headline_pos = ' '.join([str(tag) for tag in headline_pos])
@@ -138,7 +147,16 @@ def classify_clickbait(headline):
     data_tfidf = hstack([data_tfidf_pos, data_tfidf_text]).toarray()
     data_tfidf = pd.DataFrame(data_tfidf)
 
-    return int(svm.predict(data_tfidf)[0])
+    nn_pred = model.predict(data_tfidf)
+    nn_pred = [0 if i < 0.5 else 1 for i in nn_pred][0]
+
+    predictions = [int(svm.predict(data_tfidf)[0]),
+                   int(mnb.predict(data_tfidf)[0]),
+                   int(lr.predict(data_tfidf)[0]),
+                   int(rf.predict(data_tfidf)[0]),
+                   nn_pred]
+
+    return max(set(predictions), key=predictions.count)
 
 import jsonlines
 
@@ -200,7 +218,7 @@ def readall():
         article_information = OrderedDict([
                             ("id" , s[0]),
                             ("name", s[1]),
-                            ("image", s[2]),
+                            ("imageurl", s[2]),
                             ("url" , s[3]),
                             ("timestamp" , s[4]),
                             ("description" , s[5]),
